e01a7dd58a384be4404017edcb5f15fcd0129a69
destowq@gmail.com <destowq@gmail.com>
2024-10-13 11:43:22 -0400

    title updated

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101431121 || Daria Ignateva || DevOps</h5>
+      <h5>Developed By: 101431121 || Daria Ignateva || DevOps - COMP 3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
